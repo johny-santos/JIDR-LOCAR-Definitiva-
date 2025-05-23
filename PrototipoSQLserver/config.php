7a431d2c700c05d717f9b25e955a6e911515cefe
johny-santos <johnyworksplace@gmail.com>
2025-05-23 17:56:31 -0300

    Adiciona contrato e atualiza estilos e index

--- PrototipoSQLserver/config.php
+++ PrototipoSQLserver/config.php
@@ -2,18 +2,18 @@
 
 $serverName = "localhost\\AULAS";
 $connectionOptions = array(
-    "Database" => "testeConexão3",
+    "Database" => "jidr_locar",
     "UID" => "",   // deixar em branco
     "PWD" => ""    // deixar em branco
 );
 
 $conexao = sqlsrv_connect($serverName, $connectionOptions);
 
-/*
+
 if ($conexao === false) {
     die(print_r(sqlsrv_errors(), true));
 } else {
     echo 'Parabéns! Conexão realizada com êxito!';
 }
-  */  
+  
 ?>
